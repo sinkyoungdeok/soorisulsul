f7316b713644e149d99919948e0de0395526839e
sinkyoungdeok <tlsrudejr123@nate.com>
2021-02-20 17:51:58 +0900

    [ADD] (signin) kakao 로그인 기능 완성

--- lib/screen/signIn.dart
+++ lib/screen/signIn.dart
@@ -1,10 +1,102 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/all.dart';
 
 import '../route.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
+  SignIn({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _KakaoLoginState createState() => _KakaoLoginState();
+}
+
+class _KakaoLoginState extends State<SignIn> {
+  bool _isKakaoTalkInstalled = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initKakaoTalkInstalled();
+  }
+
+  _initKakaoTalkInstalled() async {
+    final installed = await isKakaoTalkInstalled();
+    print('kakao Install : ' + installed.toString());
+
+    setState(() {
+      _isKakaoTalkInstalled = installed;
+    });
+  }
+
+  _issueAccessToken(String authCode) async {
+    try {
+      var token = await AuthApi.instance.issueAccessToken(authCode);
+      AccessTokenStore.instance.toStore(token);
+      print(token);
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MainRoute()),
+      );
+    } catch (e) {
+      print("error on issuing access token: $e");
+    }
+  }
+
+  _loginWithKakao() async {
+    try {
+      var code = await AuthCodeClient.instance.request();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _loginWithTalk() async {
+    try {
+      var code = await AuthCodeClient.instance.requestWithTalk();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  logOutTalk() async {
+    try {
+      var code = await UserApi.instance.logout();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  unlinkTalk() async {
+    try {
+      var code = await UserApi.instance.unlink();
+      print(code.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
 
   String email = '';
   String password = '';
@@ -13,6 +105,8 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    isKakaoTalkInstalled();
+
     return Stack(
       children: <Widget>[
         visualContainer(),
@@ -170,6 +264,7 @@ class SignIn extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(top: 20),
       child: InkWell (
+        onTap: () => _isKakaoTalkInstalled? _loginWithTalk() : _loginWithKakao(),
         child: Container(
           width: MediaQuery.of(context).size.width * 1,
           height: MediaQuery.of(context).size.height * 0.05,
@@ -243,6 +338,8 @@ class SignIn extends StatelessWidget {
       ),
     );
   }
+
+
   
 }
 
